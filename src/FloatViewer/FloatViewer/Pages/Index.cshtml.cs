2c01c4e011b1a3d64cea3c668df26dd99395ba9c
spanishprisoner <jacek.mowinski@gmail.com>
2018-03-20 18:26:27 +0100

    Strip

--- src/FloatViewer/FloatViewer/Pages/Index.cshtml.cs
+++ src/FloatViewer/FloatViewer/Pages/Index.cshtml.cs
@@ -1,17 +1,1 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-
-namespace FloatViewer.Pages
-{
-    public class IndexModel : PageModel
-    {
-        public void OnGet()
-        {
-
-        }
-    }
-}
+﻿
--- src/FloatViewer/FloatViewer/Startup.cs
+++ src/FloatViewer/FloatViewer/Startup.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -9,7 +5,7 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace FloatViewer
 {
-    public class Startup
+	public class Startup
     {
         public Startup(IConfiguration configuration)
         {
